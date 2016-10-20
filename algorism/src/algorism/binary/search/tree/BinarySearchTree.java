a940b83c62c502e477d2b99c642d327cb1a748d4
heracul <heracul@gmail.com>
2016-10-19 23:36:29 -0700

    이중검색트리 개발

---
+++ algorism/src/algorism/binary/search/tree/BinarySearchTree.java
@@ -0,0 +1,40 @@
+package algorism.binary.search.tree;
+
+public class BinarySearchTree {
+
+	public boolean insert(int t) {
+		Node
+	}
+	public static void main(String[] args) {
+
+	}
+	
+	class Node {
+		private Node right, left;
+		private int data;
+		
+		public Node(int data) {
+			this.data = data;
+		}
+		public Node getRight() {
+			return right;
+		}
+		public void setRight(Node right) {
+			this.right = right;
+		}
+		public Node getLeft() {
+			return left;
+		}
+		public void setLeft(Node left) {
+			this.left = left;
+		}
+		public int getData() {
+			return data;
+		}
+		public void setData(int data) {
+			this.data = data;
+		}
+		
+	}
+
+}
