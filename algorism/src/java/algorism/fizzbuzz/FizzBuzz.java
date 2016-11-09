65c8834c05317c317d87bc867aa3cb1d002498bb
'heracul' <'heracul@gmail.com'>
2016-11-09 15:01:15 +0900

    ..

--- algorism/src/java/algorism/fizzbuzz/FizzBuzz.java
+++ algorism/src/java/algorism/fizzbuzz/FizzBuzz.java
@@ -2,9 +2,14 @@ package algorism.fizzbuzz;
 
 public class FizzBuzz {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public FizzBuzz() throws Exception {
+		throw new Exception("정의시 오류");
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		FizzBuzz fb = new FizzBuzz();
+		RuntimeException re = new RuntimeException();
 	}
 
 }
