acb343a5ef4b8ece119891b0da138de0213e4c28
heracul <heracul@gmail.com>
2016-11-15 22:15:09 -0800

    1.callByValue, callByReference 에 대한 테스트

--- algorism/src/test/algorism/compare/ArraySortTest.java
+++ algorism/src/test/algorism/compare/ArraySortTest.java
@@ -1,14 +1,11 @@
 package algorism.compare;
 
-import static org.junit.Assert.assertArrayEquals;
-
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import algorism.utils.BaseUtils;
 public class ArraySortTest {
 	
 	
@@ -18,22 +15,43 @@ public class ArraySortTest {
 	
 	@Test
 	public void sortIntTest() {
-		HashMap map = new HashMap();
-		ArraySort ic = new ArraySort();
-		Integer[] randomInt = BaseUtils.getRandomIntegerArrayNoDup(10, 10);
-		Integer[] sortedInt = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-		System.out.print("before:");
-		BaseUtils.printLine(randomInt);
-		ic.sortInts(randomInt);
-		System.out.print("after:");
-		BaseUtils.printLine(randomInt);
+		People people1 = new People();
+		People people2 = new People();
+		People people3 = new People();
 		
-		assertArrayEquals(sortedInt, randomInt);
+		ArrayList list = new ArrayList();
+		list.add(people1);
+		list.add(people2);
+		list.add(people3);
+		
+		System.out.println(list.size());
+		System.out.println("애드피플전 : "+list.hashCode());
+		addPeople(list);
+		System.out.println(list.size());
+				
 	}
 	
+	public void addPeople(List list) {
+		list = new ArrayList();
+//		list = createList(list);
+		System.out.println("애들피플 중 : "+list.hashCode());
+		System.out.println(list.size());
+	}
+	
+	public List createList(List list) {
+		ArrayList list1 = new ArrayList(list);
+		People people1 = new People();
+		list1.add(people1);
+		return list1;
+	}
 	@AfterClass
 	public static void tearDown() {
 		
 	}
 
+	class People {
+		String lastName;
+		String firstName;
+		int age;
+	}
 }
