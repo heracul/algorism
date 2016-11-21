7b79c98d26dd97d2532657bb2fa37f46fe52d6e3
heracul <heracul@gmail.com>
2016-11-20 18:18:08 -0800

    CSV파일에 대한 구간 read를 하도록 BufferedReader에서 FileReader로 변경하여 처리함.

--- name.finder/src/main/java/kkb/exam/manager/impl/NameFinderManager.java
+++ name.finder/src/main/java/kkb/exam/manager/impl/NameFinderManager.java
@@ -2,13 +2,13 @@ package kkb.exam.manager.impl;
 
 import java.util.Map;
 
-import kkb.exam.constants.CommonSpec;
-import kkb.exam.manager.IFinderManager;
-import kkb.exam.utils.FinderUtils;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+
+import kkb.exam.constants.CommonSpec;
+import kkb.exam.manager.IFinderManager;
+import kkb.exam.utils.FinderUtils;
 
 public class NameFinderManager implements IFinderManager {
 	
@@ -30,34 +30,35 @@ public class NameFinderManager implements IFinderManager {
 	/** 
 	 * 학교명을 Key로 하여 건수를 Map에 put할 수 있도록 처리함. 
 	 */
-	public void findWord(String readLine, Map<String, Integer>nameMap) {
-		for(String removeWord : CommonSpec.REMOVE_WORD_ARRAY.getAttribute()) {//불필요한 문자열 공백으로 치환 
-			readLine = readLine.replaceAll(removeWord, " ");
+	public void findWord(String readLine, Map<String, Integer>nameMap, boolean doDistinct) {
+		if(StringUtils.isBlank(readLine))return;
+		for(String removeWord : CommonSpec.REMOVE_WORD_ARRAY.getAttribute()) {
+			readLine = validateLetter(readLine).replaceAll(removeWord, " ");//완성된 한글만 취득 후, 불필요한 문자열 치환 
 		}
 		
 		String[]tokenArr = StringUtils.split(readLine, (char)0x20);//공백을 기준으로 split 수행 
 		Integer cnt = null;
-		String newToken;
+		String newToken = null;
 		saveToken:for(String token : tokenArr) {
-			for(String specificWord : CommonSpec.REMOVE_SEPECIFIC_WORD_ARRAY.getAttribute()) {
-				if(specificWord.equals(token.trim())) {
-					continue saveToken;
+			if(StringUtils.isNotBlank(newToken = FinderUtils.getMatchedLetter(CommonSpec.FIND_NAME_ARRAY.getAttribute(), token, true))) {
+				for(String specificWord : CommonSpec.REMOVE_SEPECIFIC_WORD_ARRAY.getAttribute()) {//특수 언어들과 완전히 일치한다면 skip하도록 처리.
+					if(specificWord.equals(newToken.trim())) {
+						continue saveToken;
+					}
 				}
-			}
-			newToken = validateLetter(token);
-			if(StringUtils.isNotBlank(newToken = FinderUtils.getMatchedLetter(CommonSpec.FIND_NAME_ARRAY.getAttribute(), newToken, true))) {
 				cnt = nameMap.get(newToken);
 				if(null != cnt) {
 					nameMap.put(newToken, cnt+1);
 				} else {
 					nameMap.put(newToken, 1);
 				}
+				if(doDistinct) break;
 			}
 		}
 	}
 	
 	/**
-	 * 문자열에 대한 validate수행 
+	 * 제대로 작성된 한글만 재 취합 후 return함. 
 	 * @param str
 	 * @return
 	 */
@@ -67,6 +68,8 @@ public class NameFinderManager implements IFinderManager {
 		for(char c : charArr) {
 			if(FinderUtils.isCompleteyKoreanLetter(c)) {
 				sb.append(c);
+			} else {
+				sb.append(" ");//완성형 한글 캐릭터가 아닐 경우에는 공백추가.
 			}
 		}
 		return sb.toString();
