3ef6db2f60654ae5272052df109e896251947689
heracul <heracul@gmail.com>
2016-11-22 23:52:09 -0800

    인터페이스 명 변경 및 클래스 명 변

---
+++ name.finder/src/main/java/kkb/exam/processor/IFindProcessor.java
@@ -0,0 +1,27 @@
+package kkb.exam.processor;
+
+import java.util.Map;
+
+public interface IFindProcessor {
+
+	/**
+	 * 인스턴스 초기화를 위한 메소드 
+	 */
+	public abstract void init();
+	
+	/**
+	 * String에서 학교명을 찾기위한 메소드
+	 * @param readLine
+	 * @param nameMap
+	 * @param doDistinct false일 경우에는 readLine내 학교명에 대한 카운트중복을 허용한다. 
+	 */
+	public abstract void findWord(String readLine, Map<String, Integer>nameMap, boolean doDistinct);
+	
+	/**
+	 * 문자열에 대한 validation을 수행한다. (ex. 정상적인 한글문자여부 확인)
+	 * @param str
+	 * @return
+	 */
+	public abstract String validateLetter(String str);
+	
+}
